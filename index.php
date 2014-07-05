6c81fed05efbf7f259bcd9d6db095c5b11250d86
Shantanu Tushar <shantanu@kde.org>
2014-07-05 11:52:06 +0530

    Add some logos and footer text

--- index.php
+++ index.php
@@ -41,7 +41,8 @@
   <body role="document">
     <div class="container">
       <div class="starter-template col-md-6 col-md-offset-3">
-        <h1 class="title">Easily migrate your Drupal posts to <a href="http://ghost.org">Ghost</a></h1>
+        <h1 class="title">Easily migrate your <a href="http://drupal.org">Drupal</a> posts to <a href="http://ghost.org">Ghost</a></h1>
+        <img src="drupal-to-ghost.png" width="99%" />
         <hr />
         <ol class="steps">
           <li><p class="lead">Install the <a href="https://drupal.org/project/data_export_import">data_export_import</a> Drupal module</p></li>
@@ -54,9 +55,21 @@
             <div class="lead col-md-6">Nodes dataset: <input class="btn btn-lg btn-default" type="file" name="nodesDataset"></div>
             <div class="lead col-md-6">Taxonomy dataset: <input class="btn btn-lg btn-default" type="file" name="taxonomyDataset"></div>
           </div>
-          <input class="btn btn-lg btn-default" type="submit">
+          <input class="btn-primary btn btn-lg btn-default" type="submit" value="Convert">
         </form>
+
+        <hr />
+        <div class="well">
+          <ul>
+            <li><a href="http://ghost.org">Ghost</a> doesn't support importing users (yet)</li>
+            <li>Your data is not stored on the server, everything is processed in-memory</li>
+            <li>Brought to you by <a href="http://www.shantanutushar.com">Shantanu Tushar</a> and <a href="https://github.com/dpacmittal">Deepak Mittal</a></li>
+            <li>Drupal is a <a href="http://drupal.com/trademark" rel="nofollow">registered trademark</a> of <a href="http://buytaert.net" rel="nofollow">Dries Buytaert</a></li>
+          </ul>
+        </div>
       </div>
+
+    <a href="https://github.com/shaan7/ghost-drupal-import"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
 
     </div>
     <!-- Bootstrap core JavaScript
